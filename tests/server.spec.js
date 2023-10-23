1f2cd9a6203f1c601fdaf61fb0fe1d097a7306f4
josemfiguero04 <josemfigueroa04@gmail.com>
2023-10-23 19:13:15 -0300

    feature test get

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,13 @@
 const request = require("supertest");
 const server = require("../index");
+const e = require("express");
+
 
 describe("Operaciones CRUD de cafes", () => {
+    it('GET /cafes debe devolver un status 200 y un array de cafes', async () => {
+        const res = await request(server).get("/cafes");
+        expect(res.statusCode).toEqual(200);
+        expect(Array.isArray(res.body)).toBe(true);        
 
-});
+})
+})
